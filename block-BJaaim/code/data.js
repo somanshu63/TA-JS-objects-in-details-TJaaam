a3ede329822eb7083aff5b14c9851b315c6e5f45
somanshu63 <somanshu63@gmail.com>
2022-07-27 10:11:23 +0530

    learn quiz app by OOP

---
+++ block-BJaaim/code/data.js
@@ -0,0 +1,27 @@
+quizCollection = [
+    question = {
+        title: "You're 4th place right now in a race. What place will you be in when you pass the person in 3rd place?",
+        options: ["1st", "2nd", "3rd", "none"],
+        correctAnswer: 1
+    },
+    question = {
+        title: "How many months have 28 days?",
+        options: ["2", "1", "all", "depends on leap year"],
+        correctAnswer: 2
+    },
+    question = {
+        title: "The answer is really big. ",
+        options: ["THE ANSWER.", "Really big.", "An elephant.", "The data given is insufficient."],
+        correctAnswer: 1
+    },
+    question = {
+        title: "Divide 30 by half and add ten. ",
+        options: ["40.5", "50", "70", "I know this is a trick question, so NONE. Ha!"],
+        correctAnswer: 0
+    },
+    question = {
+        title: "Jimmy's father has three sons- Paul I and Paul II. Can you guess the name of the third son? ",
+        options: ["Paul III", "Jerin", "Jimmy", "none"],
+        correctAnswer: 0
+    },
+]
